37a1e1066a1351555f1919a4049aac5fbee826b2
157888390 <157888390@users.noreply.github.com>
2026-07-25 18:30:34 +0800

    chore(strmscraper): 版本升至 v1.1.0 并维护 history，使已安装插件可检测更新
    - package.v2.json version 1.0.0 -> 1.1.0，新增 history（v1.1.0/v1.0.1/v1.0.0）
    - __init__.py plugin_version 同步升至 1.1.0
    - 描述补充'补齐剧集级元数据(tvshow.nfo/海报等)'
    - 注：MoviePilot 以 package.v2.json 的 version 与已装插件 plugin_version 做语义比较判定更新

--- plugins.v2/strmscraper/__init__.py
+++ plugins.v2/strmscraper/__init__.py
@@ -52,7 +52,7 @@ class StrmScraper(_PluginBase):
     # 插件图标
     plugin_icon = "strmscraper.png"
     # 插件版本
-    plugin_version = "1.0.0"
+    plugin_version = "1.1.0"
     # 插件作者
     plugin_author = "157888390"
     # 作者主页
